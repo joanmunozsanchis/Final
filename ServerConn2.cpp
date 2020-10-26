324c7596b0763cd6e20f73d7692c4c41714adde8
Joan Muñoz Sanchis <joan.munoz.sanchis@estudiantat.upc.edu>
2020-10-26 23:27:27 +0100

    Ej Guia Con los 2 Retos Añadidos

---
+++ ServerConn2.cpp
@@ -0,0 +1,76 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[])
+{
+	
+	int sock_conn, sock_listen, ret;
+	struct sockaddr_in serv_adr;
+	char peticion[512];
+	char respuesta[512];
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	// Fem el bind al port
+	
+	
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	
+	// asocia el socket a cualquiera de las IP de la m?quina. 
+	//htonl formatea el numero que recibe al formato necesario
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// establecemos el puerto de escucha
+	serv_adr.sin_port = htons(9700);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	
+	if (listen(sock_listen, 3) < 0)
+		printf("Error en el Listen");
+	
+	int i;
+	// Bucle infinito
+	for (;;){
+		printf ("Escuchando\n");
+		
+		sock_conn = accept(sock_listen, NULL, NULL);
+		printf ("He recibido conexion\n");
+		//sock_conn es el socket que usaremos para este cliente
+		
+		int terminar =0;
+		// Entramos en un bucle para atender todas las peticiones de este cliente
+		//hasta que se desconecte
+		while (terminar ==0)
+		{
+			// Ahora recibimos la petici?n
+			ret=read(sock_conn,peticion, sizeof(peticion));
+			printf ("Recibido\n");
+			
+			// Tenemos que a?adirle la marca de fin de string 
+			// para que no escriba lo que hay despues en el buffer
+			peticion[ret]='\0';
+			char resultado[300];
+			
+			printf ("Peticion: %s\n",peticion);
+			
+			int celsius = atoi(peticion);
+			int fahr;
+			fahr = celsius + 32;
+			printf("Su temperatura en Fahrenheit es: %d\n",fahr);
+			sprintf(resultado,"%d",fahr);
+
+
+			write (sock_conn,resultado, strlen(resultado));
+			close(sock_conn); 
+			terminar = 1;
+		}
+
+		close(sock_conn); 
+	}
+}
+
